77b6f8ad6ef24e565bf268b73f5bb60b0bf79adf
Meghna Agrawal <meghnaagrawal10@gmail.com>
2024-12-17 14:26:00 -0500

    firebase error checking during initialization

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./index.css";
@@ -10,6 +10,10 @@ import {
   ApolloProvider,
 } from "@apollo/client";
 
+import { initializeApp } from "firebase/app";
+
+import fbConfig from "./firebase/FirebaseConfig";
+
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   link: new HttpLink({
@@ -17,16 +21,50 @@ const client = new ApolloClient({
   }),
 });
 
-import fbConfig from "./firebase/FirebaseConfig";
-console.log(fbConfig);
-import { initializeApp } from "firebase/app";
+// console.log(fbConfig);
+// const app = initializeApp(fbConfig);
 
-const app = initializeApp(fbConfig);
+const RootComponent = () => {
+  const [firebaseInitialized, setFirebaseInitialized] = useState(false);
+  const [firebaseError, setFirebaseError] = useState(null)
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ApolloProvider client={client}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ApolloProvider>
-);
+  useEffect(() => {
+    try {
+      if (!fbConfig.apiKey){
+        throw "Api key not provided!"
+      }
+      const app = initializeApp(fbConfig)
+      setFirebaseInitialized(true)
+      console.log(app)
+    } catch (error) {
+      console.error("Firebase initialization error!");
+      setFirebaseError("Firebase is not available at the moment. Please try again later.")
+    }
+  }, [])
+
+  if (!firebaseInitialized) {
+    return <div>Provide Firebase API Key</div>
+  }
+
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        {firebaseError ? (
+          <div className="config-error" style={{color: 'white'}}>{firebaseError}</div>
+        ) : (
+          <App />
+        )}
+      </BrowserRouter>
+    </ApolloProvider>
+  );
+};
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <ApolloProvider client={client}>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </ApolloProvider>
+// );
+
+ReactDOM.createRoot(document.getElementById("root")).render(<RootComponent />);
